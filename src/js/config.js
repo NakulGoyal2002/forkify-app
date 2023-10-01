ee4cfe20b5685a38988cade5a81bd35290d6e424
Nakul Goyal <nakul.21239@knit.ac.in>
2023-10-01 11:57:25 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4c350fad-b322-4a7a-b6a4-78bc49ba3e91';
+export const MODAL_CLOSE_SEC = 2.5;
